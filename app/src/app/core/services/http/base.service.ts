f67dc0860a71008baa0403989ec8a7ad906e9a33
Edwar Zapata <edwar.zapata@utp.edu.co>
2021-07-26 09:20:40 -0500

    actualización de rutas

--- app/src/app/core/services/http/base.service.ts
+++ app/src/app/core/services/http/base.service.ts
@@ -3,10 +3,10 @@ import { HttpClient } from '@angular/common/http';
 export class BaseService {
 
   protected http: HttpClient;
-  protected url = 'http://localhost:3000/api/v1';
+  // protected url = 'http://localhost:3010/api/v1';
   // protected url = 'http://192.168.32.111/api/ticinventory';
   // protected url = 'http://10.1.1.197:8080/api/ticinventory';
-  // protected url = 'http://app4.utp.edu.co/api/ticinventory';
+  protected url = 'http://app4.utp.edu.co/api/v1';
 
   constructor(httpClient: HttpClient) {
     this.http = this.httpClient;
